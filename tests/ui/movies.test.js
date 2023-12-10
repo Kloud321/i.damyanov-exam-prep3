0b70e7b46499a0eed7ff8a1d4fbff6332824b1f1
IvanD <damyanov.dimitrov.ivam@gmail.com>
2023-12-10 09:45:25 +0200

    fix the movie test

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-    await page.goto('http://i-damyanov-exam-prep3-softuni.onrender.com');
+    await page.goto('http://i-damyanov-exam-prep3-softuni.onrender.com/movies');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
